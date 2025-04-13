9fea8cc15b18829f1a1e2d170565a99801265465
ANDRIOSM <andres4715@gmail.com>
2025-04-13 11:20:29 -0500

    initial commit 21

--- src/pages/SearchPage.ts
+++ src/pages/SearchPage.ts
@@ -1,5 +1,5 @@
 import BasePage from './BasePage';
-import {Page, expect} from '@playwright/test';
+import {Page, expect, Locator} from '@playwright/test';
 
 export default class SearchPage extends BasePage {
   // Selectors
@@ -21,6 +21,7 @@ export default class SearchPage extends BasePage {
 
   async clickAddToCartButton(): Promise<void> {
     await this.page.waitForTimeout(2000);
+    await expect(this.page.locator(this.productAddToCartButton)).toBeVisible();
     await this.click(this.productAddToCartButton);
   }
 }
