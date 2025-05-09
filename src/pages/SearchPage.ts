fe376584665252c474a23915eeced6b90486b737
ariosm <ariosm@ariosms-MacBook-Pro.local>
2025-05-09 14:07:09 -0500

    new data locators

--- src/pages/SearchPage.ts
+++ src/pages/SearchPage.ts
@@ -2,7 +2,7 @@ import BasePage from './BasePage';
 import {Page, expect, Locator} from '@playwright/test';
 
 export default class SearchPage extends BasePage {
-  // Selectors
+  // locators
   private readonly searchTitle = '.page-title h1';
   private readonly productAddToCartButton =
     "button[type='button']:nth-child(1)";
